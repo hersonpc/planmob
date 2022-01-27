c0e088fd07b3ae7b8074366428b8d49dba88a7c4
Herson Melo <hersonpc@gmail.com>
2022-01-27 04:51:29 -0300

    Leitura inicial do shapefile

---
+++ src/01_data_acquisition.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(raster)
+shp <- shapefile("./src/shapefiles/goiania_regiao_metropolitana/goiania_regiao_metropolitana")
+
+plot(shp, col="#f2f2f2", bg="gray", lwd=0.95, border=1)
+
+ggplot(data = shp) + 
+    aes(x = long, 
+        y = lat, 
+        group = group) +
+    geom_polygon(
+        colour = "black",
+        fill = NA) +
+    theme_void()
+
+# shp_df <- broom::tidy(shp, region = "CD_GEOCODM")
+# lapply(shp_df, class)
+# 
+# ggplot() + 
+#     geom_sf(data = shp, size = 3, color = "black", fill = "cyan1") + 
+#     ggtitle("AOI Boundary Plot") + 
+#     coord_sf()
+# 
+# library(broom)
+# shp_fortified <- tidy(shp, region = "NAMES")
+# 
+# # Plot it
+# library(ggplot2)
+# ggplot() +
+#     geom_polygon(data = shp_fortified, aes( x = long, y = lat, group = group), fill="#69b3a2", color="white") +
+#     theme_void() 
